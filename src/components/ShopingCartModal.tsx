f2b1d51da586fc164240de6457b0a552ea53c7d0
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-26 11:49:29 +0100

    Refactor ShopingCartModal component and update dependencies

--- src/components/ShopingCartModal.tsx
+++ src/components/ShopingCartModal.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import {
   Sheet,
@@ -9,7 +9,7 @@ import {
 import { useShoppingCart } from "use-shopping-cart";
 
 export default function ShopingCartModal() {
-    const {cartCount} = useShoppingCart();
+  const { cartCount } = useShoppingCart();
   return (
     <Sheet defaultOpen>
       <SheetContent className="sm:max-w-lg w-[90vw]">
@@ -18,13 +18,13 @@ export default function ShopingCartModal() {
         </SheetHeader>
         <div className="h-full flex flex-col justify-between">
           <div className="mt-8 flex-1 overflow-y-auto">
-<ul className="-my-6 divide-y divide-gray-200">
-{cartCount === 0 ? (
-<h1 className="">No items in the cart</h1>
-):(
-<h1>You have items in the cart</h1>
-)}
-</ul>
+            <ul className="-my-6 divide-y divide-gray-200">
+              {cartCount === 0 ? (
+                <h1 className="text-3xl py-6">No items in the cart</h1>
+              ) : (
+                <h1>You have items in the cart</h1>
+              )}
+            </ul>
           </div>
         </div>
       </SheetContent>
