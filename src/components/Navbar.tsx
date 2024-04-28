51fc894a107a8f83f79435146b008d3b52344b6e
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-28 13:42:01 +0100

    Refactor Navbar component to update handleCartClick and cartCount logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,27 @@ import { ShoppingCart, X } from "lucide-react";
 import { Button } from "./ui/button";
 import { useShoppingCart } from "use-shopping-cart";
 
-function Navbar() {
 
-const{handleCartClick} = useShoppingCart();
+interface NavbarProps { 
+  cartCount: number;
+  handleCartClick: () => void;
+}
+
+
+
+function Navbar() {
+  const {
+    cartCount
+  } = useShoppingCart();
+  const { handleCartClick } = useShoppingCart();
   return (
     <div className="flex items-center justify-end h-20 pr-4 mb-10 bg-primary">
-        <Button className="dropdown-icons" variant='default' 
+      <Button variant='default'
         onClick={() => handleCartClick()}
-        >
-          <ShoppingCart className="text-orange-500"/>
-        </Button>
+      >
+        <ShoppingCart className="text-orange-500" />
+        {(cartCount ?? 0) > 0 && <span className="absolute text-white mb-4 ml-8">{cartCount}</span>}
+      </Button>
     </div>
   );
 }
